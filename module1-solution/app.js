6134da88b45d0d392c3b0a1bbaf288d95279d75c
Zouhair Boudahri <bouda.zouhair@gmail.com>
2017-04-30 16:27:11 +0100

    initial commit

---
+++ module1-solution/app.js
@@ -0,0 +1,37 @@
+(function (){
+  'use strict';
+  var module = angular.module('LunchCheck', []);
+  module.controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope){
+    $scope.items = "";
+    $scope.checkResult = "";
+    $scope.checkResultStyle = "";
+    $scope.checkIfTooMuch = checkIfTooMuch;
+
+    function checkIfTooMuch(){
+      var itemsArray = $scope.items.split(',');
+      var itemsCount = 0;
+
+      itemsArray.forEach(function (item){
+        if(item != null && item != "" && item != " "){ // NOT considering empty items
+          itemsCount++;
+        }
+      });
+
+      if(itemsCount == 0) {
+          $scope.checkResult = "Please enter data first";
+          $scope.checkResultStyle = "alert alert-danger";
+      } else if (itemsCount <= 3) {
+          $scope.checkResult = "Enjoy!";
+          $scope.checkResultStyle = "alert alert-success";
+      } else {
+          $scope.checkResult = "Too much";
+          $scope.checkResultStyle = "alert alert-success";
+      }
+
+    }
+  }
+})();
